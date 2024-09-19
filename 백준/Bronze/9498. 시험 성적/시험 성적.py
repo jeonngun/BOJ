cbfeb738413ba41b511cc5bf85bcd44eb2b62ed0
jeonngun <dnss0102@gmail.com>
2024-09-19 16:48:04 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,11 @@
+exam = int(input())
+if 90 <= exam <= 100:
+    print("A")
+elif 80 <= exam < 90:
+    print("B")
+elif 70 <= exam < 80:
+    print("C")
+elif 60 <= exam < 70:
+    print("D")
+else:
+    print("F")
